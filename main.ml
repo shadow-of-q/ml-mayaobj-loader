df1eba285a2c3a814764bfce77102e8e51138484
shadow.of.q <shadow.of.q@gmail.com>
2013-08-29 00:10:55 +0100

    Added missing files

---
+++ main.ml
@@ -0,0 +1,10 @@
+open Mayaobjparser
+open Mayaobj
+
+let scene = Mayaobjparser.loadobj "conference.obj";;
+
+Printf.printf "vertices %d\ntriangles %d\nmaterials %d\n"
+  (Array.length scene.vertices)
+  (Array.length scene.triangles)
+  (Array.length scene.materials)
+
